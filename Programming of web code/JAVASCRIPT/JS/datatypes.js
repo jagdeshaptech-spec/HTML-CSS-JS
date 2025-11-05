1bcc67348cc779e5cd34d01a0368227fa03f4761
jagdeshaptech-spec <jagdeshaptech@gmail.com>
2025-11-05 17:47:33 +0545

    JS DataTypes

---
+++ Programming of web code/JAVASCRIPT/JS/datatypes.js
@@ -0,0 +1,11 @@
+let lassaa = 'go' //String
+console.log(lassaa)
+
+let Id = 5006 //Integer
+console.log(Id)
+
+let you =false //Boolean
+console.log(you)
+
+let items = ["Food", "Petrol", "Rackets"] //Array
+console.log(items[1])
